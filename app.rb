d1262e867d865605f48df04bcd04e6716937b672
Austin Wolfe <wolfe_austin@yahoo.com>
2018-04-02 13:59:11 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get('/name'){"My name is Austin Wolfe"}
+  get('/hometown'){"My hometown is Johnson City, TN"}
+  get('/favorite-song'){"My favorite song is The Ballad of Curtis Loew"}
+
 end
