cdef645b548f409ff5e3454e62e5f1f6f3319edc
mkenane <morkenan@hotmail.com>
2018-04-03 08:51:12 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do
+  "My name is Mor"
 end
+
+get '/hometown' do
+  "My hometown is Rechovot"
+end
+
+get '/favorite-song' do
+  "My favorite song is not one song, it's a collection of all time favorites"
+end
+
+
+end
+
+
+
+
+#My hometown is __", and the favorite-song route should display "My favorite song is __".
